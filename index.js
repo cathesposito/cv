21a3a024f18de72a84e5e87e7c0979881006b753
cathesposito <73259833+cathesposito@users.noreply.github.com>
2021-04-03 21:51:49 -0300

    6 version

--- index.js
+++ index.js
@@ -1,39 +1,83 @@
+// Home Page
+
+function position(i) {
+
+  var diploma = ".diploma" + i;
+  var addClass = ".addClassIcon" + i;
+  var hide = ".hide" + i;
+
+  $(diploma)
+    .hover(function() {
+      $(addClass).toggleClass("iconHover");
+      $(this).css("font-size", "1.75rem").css("cursor", "pointer");
+    })
+    .click(function() {
+      $(hide).toggle("slow");
+    });
+
+};
+
+for (i = 1; i <= 4; i++) {
+  position(i)
+};
 
 
-$(".diploma1 h3").click(function(){
-  $(".hide1").toggle("slow");
+
+// Table in Home Page
+$(".tableSkills td").hover(function() {
+  $(this).toggleClass("rowColor");
 });
 
-$(".diploma2 h3").click(function(){
-  $(".hide2").toggle("slow");
-});
-
-$(".diploma3 h3").click(function(){
-  $(".hide3").toggle("slow");
-});
-
-$(".diploma4 h3").click(function(){
-  $(".hide4").toggle("slow");
-});
-
-$(".click1").click(function(){
-  $(".hide5").toggle("slow");
-});
-
-$(".click2").click(function(){
-  $(".hide6").toggle("slow");
-});
-
-$(".tableSkills td").hover(function(){
-  $(this).fadeOut(80);
-  $(this).fadeIn(50);
-});
-
+// Hover Bold in email and address
 $(".hoverB, .hoverB2")
-  .on("mouseenter", function(){
+  .on("mouseenter", function() {
     $(this).css("font-weight", "bold");
   })
-  .on("mouseleave", function(){
+  .on("mouseleave", function() {
     $(this).css("font-weight", "normal");
-  })
-;
+  });
+
+
+// Publications Page
+
+function position2(i) {
+
+  var click = ".click" + i;
+  j = 4 + i;
+  var hideP = ".hide" + j;
+
+  $(click).click(function() {
+    $(hideP).toggle("slow");
+  });
+
+};
+
+for (i = 1; i <= 2; i++) {
+  position2(i)
+};
+
+
+
+// Work Page
+
+function position3(i) {
+
+  var work = ".work" + i;
+  var addClass = ".addClassIcon" + i;
+  k = 6 + i;
+  var hide = ".hide" + k;
+
+  $(work)
+    .hover(function() {
+      $(addClass).toggleClass("iconHover");
+      $(this).css("font-size", "1.75rem").css("cursor", "pointer");
+    })
+    .click(function() {
+      $(hide).toggle("slow");
+    });
+
+};
+
+for (i = 1; i <= 6; i++) {
+  position3(i)
+};
